ac71e8e8b2cdc71cfbb5cb5d7940abcb253c369d
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2021-03-21 17:20:01 +0900

    Add vim deployment.

---
+++ src/conf/app/vim/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+set -e
+
+readonly SCRIPT_PATH=$(
+    self=${0}
+    while [ -L "${self}" ]; do
+        cd "${self%/*}"
+        self=$(readlink "${self}")
+    done
+    cd "${self%/*}"
+    echo "$(pwd -P)/${self##*/}"
+)
+readonly SCRIPT_ROOT="$(dirname ${SCRIPT_PATH})"
+readonly VIMRC_PATH="${HOME}/.vimrc"
+
+ln -snvf "${SCRIPT_ROOT}/rc.vim" ${VIMRC_PATH}
