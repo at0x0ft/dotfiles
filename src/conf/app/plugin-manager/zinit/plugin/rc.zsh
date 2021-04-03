886fa12347c735f7b98696ce72055a301727364d
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2021-04-03 16:50:46 +0900

    Update zinitrc.zsh.

--- src/conf/app/plugin-manager/zinit/plugin/rc.zsh
+++ src/conf/app/plugin-manager/zinit/plugin/rc.zsh
@@ -1,8 +1,3 @@
-# Load history and directory snippet from Prezto modules
-# zinit ice svn
-# zinit snippet PZTM::history
-
-# zinit ice wait lucid blockf
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
 zinit ice wait lucid
 zinit light Aloxaf/fzf-tab
--- src/conf/app/plugin-manager/zinit/theme/rc.zsh
+++ src/conf/app/plugin-manager/zinit/theme/rc.zsh
@@ -1,3 +1,7 @@
 # zsh-users/fast-syntax-highlighting
 zinit ice wait lucid atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay"
 zinit light zdharma/fast-syntax-highlighting
+
+# powerlevel10k
+zinit ice depth=1
+zinit light romkatv/powerlevel10k
