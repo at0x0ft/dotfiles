65b03ee88cc1b7c10400cfd141f28b3879b45e19
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2021-04-04 23:49:36 +0900

    Delete unnecessary scripts.

--- src/current/pref/has-user-shell-key.sh
+++ src/current/pref/has-user-shell-key.sh
@@ -12,6 +12,6 @@ readonly SCRIPT_PATH=$(
 )
 readonly SCRIPT_ROOT="$(dirname ${SCRIPT_PATH})"
 readonly PREFERENCE_PATH="${SCRIPT_ROOT}/preference.json"
-readonly JQ_QUERY='.package[] | [ .name, .with ] | join("/")'
+readonly JQ_QUERY='has("user_shell")'
 
 jq -r "${JQ_QUERY}" "${PREFERENCE_PATH}"
