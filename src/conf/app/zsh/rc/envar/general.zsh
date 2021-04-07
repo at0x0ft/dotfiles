222cd202f7ece0f77358e5bace4e1db0e7d42ef8
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2021-04-07 18:50:10 +0900

    Add history settings.

--- src/conf/app/zsh/rc/envar/general.zsh
+++ src/conf/app/zsh/rc/envar/general.zsh
@@ -1,2 +1,7 @@
 # '/' regard as a word delimiter.
 export WORDCHARS=${WORDCHARS/\//}
+
+# Set history variables (cite from https://github.com/sorin-ionescu/prezto/blob/master/modules/history/init.zsh)
+HISTFILE="${HISTFILE:-${ZDOTDIR:-$HOME}/.zhistory}"  # The path to the history file.
+HISTSIZE=10000                   # The maximum number of events to save in the internal history.
+SAVEHIST=10000                   # The maximum number of events to save in the history file.
