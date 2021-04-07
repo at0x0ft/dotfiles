0839e0eaf2bf45b59d8362a5b4e56c16d72df61a
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2021-04-07 18:04:07 +0900

    Delete later ignored files.

--- src/conf/app/zinit/completion/rc.zsh
+++
@@ -1,2 +0,0 @@
-source "/home/at0x0ft/.dotfiles/src/conf/app/zinit/completion/general.zsh"
-source "/home/at0x0ft/.dotfiles/src/conf/app/zinit/completion/external.zsh"
--- src/conf/app/zinit/theme/rc.zsh
+++
@@ -1,3 +0,0 @@
-source "/home/at0x0ft/.dotfiles/src/conf/app/zinit/theme/fast-syntax-highlighting.zsh"
-source "/home/at0x0ft/.dotfiles/src/conf/app/zinit/theme/p10k-load.zsh"
-source "/home/at0x0ft/.dotfiles/src/conf/app/zinit/theme/p10k.zsh"
