fcc64b111c30d77357bb52e939ec224d5674d697
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2021-04-15 16:00:46 +0900

    Temporarily resolve direnv source problem.

--- src/conf/app/zinit/plugin/rc.zsh
+++ src/conf/app/zinit/plugin/rc.zsh
@@ -14,16 +14,24 @@ function ddiff() {
     diff -u ${1} ${2} | delta
 }
 
-# zinit ice wait lucid from"gh-r" as"program" mv"direnv* -> direnv" \
-#     atclone"./direnv hook zsh > zhook.zsh" atpull"%atclone" \
-#     pick"direnv" src"zhook.zsh"
-# zinit load direnv/direnv
 # zinit ice wait lucid from"gh-r" as"program" mv"direnv* -> direnv" atclone"./direnv hook zsh > zhook.zsh" atpull"%atclone" pick"direnv" src"zhook.zsh"
-# zinit load direnv/direnv
-
-# zinit ice wait lucid from"gh-r" as"program" mv"direnv* -> direnv" atclone"./direnv hook zsh > zhook.zsh" atpull"%atclone" pick"direnv" src"zhook.zsh"
-zinit ice wait lucid from"gh-r" mv"direnv* -> direnv" sbin"direnv* -> direnv" atclone"./direnv hook zsh > zhook.zsh" atpull"%atclone" pick"direnv" src"zhook.zsh"
+zinit ice wait lucid from"gh-r" mv"direnv* -> direnv" sbin"direnv* -> direnv"
 zinit load direnv/direnv
+# start direnv source ( = zhook.zsh)
+_direnv_hook() {
+  trap -- '' SIGINT;
+  eval "$("${HOME}/.zinit/polaris/bin/direnv" export zsh)";
+  trap - SIGINT;
+}
+typeset -ag precmd_functions;
+if [[ -z ${precmd_functions[(r)_direnv_hook]} ]]; then
+  precmd_functions=( _direnv_hook ${precmd_functions[@]} )
+fi
+typeset -ag chpwd_functions;
+if [[ -z ${chpwd_functions[(r)_direnv_hook]} ]]; then
+  chpwd_functions=( _direnv_hook ${chpwd_functions[@]} )
+fi
+# end direnv source
 
 zinit ice wait lucid from"gh-r" sbin"fzf -> fzf"
 zinit load junegunn/fzf
