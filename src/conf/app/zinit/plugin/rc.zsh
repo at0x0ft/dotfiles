4310ae24e2aa739ff8e75523fa5b39b608a7f885
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2022-01-10 18:53:51 +0900

    fzf preview updated.

--- src/conf/app/zinit/plugin/rc.zsh
+++ src/conf/app/zinit/plugin/rc.zsh
@@ -8,10 +8,13 @@ alias ls='lsd --color=always'
 zinit ice wait lucid from"gh-r" mv"bat* -> bat" sbin"**/bat(.exe|) -> bat"
 zinit load sharkdp/bat
 
+zinit ice wait lucid from"gh-r" mv"fd* -> fd" sbin"**/fd(.exe|) -> fd"
+zinit load sharkdp/fd
+
 zinit ice wait lucid from"gh-r" mv"delta* -> delta" sbin"**/delta(.exe|) -> delta"
 zinit load dandavison/delta
 function ddiff() {
-    diff -u ${1} ${2} | delta --side-by-side
+    diff -u "${1}" "${2}" | delta --side-by-side
 }
 
 # zinit ice wait lucid from"gh-r" as"program" mv"direnv* -> direnv" atclone"./direnv hook zsh > zhook.zsh" atpull"%atclone" pick"direnv" src"zhook.zsh"
@@ -39,12 +42,20 @@ zinit ice wait lucid from"gh-r" sbin"fzf -> fzf"
 zinit load junegunn/fzf
 zinit ice wait lucid pick"shell/completion.zsh" id-as"junegunn/fzf_completion"
 zinit load junegunn/fzf
+export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_COMPLETION_OPTS='--preview '"'"'bat {} >/dev/null 2>&1; if [[ "${?}" == 0 ]]; then bat --color=always {}; else lsd -1 --color=always {}; fi'"'"
 
+function dcd() {
+    # ref: https://stackoverflow.com/questions/55186799/multi-process-bash-within-fzf-preview-feature
+    # ref (how to embed tab character) : https://mattintosh.hatenablog.com/entry/2013/01/16/143323
+    local dirs_number=$(dirs -v | fzf --preview 'readonly selected={} && eval "lsd -1 --color=always ${selected#*$'"'"'\t'"'"'}"' | cut -f 1)
+    if [[ ! -z "${dirs_number}" ]]; then cd +"${dirs_number}"; fi
+}
 
 zinit ice wait lucid blockf
 zinit load Aloxaf/fzf-tab
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'lsd -1 --color=always ${(Q)realpath}'
-zstyle ':fzf-tab:complete:lsd:*' fzf-preview 'bat --color=always ${(Q)realpath}'
+zstyle ':fzf-tab:complete:lsd:*' fzf-preview 'bat ${(Q)realpath} >/dev/null 2>&1; if [[ "${?}" == 0 ]]; then bat --color=always ${(Q)realpath}; else lsd -1 --color=always ${(Q)realpath}; fi'
 zstyle ':fzf-tab:complete:(-command-|-parameter-|-brace-parameter-|export|unset|expand):*' fzf-preview 'echo ${(P)word}'
 zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview 'git diff $word | delta --side-by-side'|
 zstyle ':fzf-tab:complete:git-log:*' fzf-preview 'git log --color=always $word'
--- src/conf/app/zsh/rc/opt/general.zsh
+++ src/conf/app/zsh/rc/opt/general.zsh
@@ -10,3 +10,9 @@ setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
 setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
 setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
 setopt HIST_BEEP                 # Beep when accessing non-existent history.
+
+# Cite from https://github.com/sorin-ionescu/prezto/blob/master/modules/directory/init.zsh
+setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
+setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
+setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
+setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
